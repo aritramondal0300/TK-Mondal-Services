fbeb967d516c6ddac3d59671b7dc8f471f597078
aritramondal0300 <mondal.aritra2024@gmail.com>
2026-05-16 14:33:31 +0530

    fresh tree clean

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+
   basePath: '/TK-Mondal-Services',
   assetPrefix: '/TK-Mondal-Services/',
+
   images: {
     unoptimized: true,
+  },
+
+  experimental: {
+    turbo: {
+      rules: {},
+    },
   },
 };
 
